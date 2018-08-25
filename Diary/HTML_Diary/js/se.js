4be746489bb4d928f0b70ba03ca7d9511bc4ca39
TomiokaRio <riopf239@gmail.com>
2018-08-26 01:44:04 +0900

    2018/08/26 01:43

--- Diary/HTML_Diary/js/se.js
+++ Diary/HTML_Diary/js/se.js
@@ -1,3 +1,60 @@
-function ring() {
-    document.getElementById("SoundTest").play();
+// fireflower: 1,20
+function fireflowerStart(){
+    var v = document.getElementById("fireflower");
+    v.volume = 1;
+    v.load();
+    v.play();
 }
+function fireflowerStop(){
+    var v = document.getElementById("fireflower");
+    v.volume = v.volume - 1; // volumeを0にする
+}
+function fireflowerReStart(){
+    var v = document.getElementById("fireflower");
+    v.volume = 1;
+}
+
+
+
+
+
+// enjoy: 20,40
+function enjoyStart(){
+    var v = document.getElementById("enjoy");
+    v.volume = 1;
+    v.load();
+    v.play();
+}
+function fireflowerStop(){
+    var v = document.getElementById("enjoy");
+    v.volume = v.volume - 1;
+}
+function fireflowerReStart(){
+    var v = document.getElementById("enjoy");
+    v.volume = 1;
+}
+
+
+
+
+
+// sad: 40,60
+function sadStart(){
+    var v = document.getElementById("sad");
+    v.volume = 1;
+    v.load();
+    v.play();
+}
+function fireflowerStop(){
+    var v = document.getElementById("sad");
+    v.volume = v.volume - 1;
+}
+function fireflowerReStart(){
+    var v = document.getElementById("sad");
+    v.volume = 1;
+}
+
+
+
+
+
---
+++ Diary/soundDiary/js/switch.js
@@ -0,0 +1,6 @@
+function LightSky() {
+    document.getElementById("sky").className = "daytime";
+}
+function DarkSky() {
+    document.getElementById("sky").className = "night";
+}
